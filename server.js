316b297bad6ecb83e7cb96f0626110ac437130c7
Alexander Leal <alex.j.leal@live.com>
2021-03-07 21:44:31 -0700

    google for books

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-
+// Add routes, both API and view
 app.use(routes);
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
-
-mongoose.connect(MONGODB_URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
